57296b1fafc8ba70b24315a7e12d77ab3bf7f11c
Kyle Welsby <kyle@mekyle.com>
2011-08-07 02:42:31 +0100

    Threads arrays is now ready

--- javascripts/thread.js
+++ javascripts/thread.js
@@ -1,7 +1,24 @@
 var Thread = (function(){
     return {
-        list: function(recipient){
-            var all_messages = $.parseJSON(localStorage["messages"]),
+        list: function(){
+            var all_messages = $.parseJSON(localStorage.messages),
+            people = [];
+            conversations = [];
+            $.each(all_messages,function(i){
+                var message = this;
+                people.push(message.message.recipient.toString());
+                
+            });
+            people = $.unique(people);
+            
+            $.each(people, function(){
+                conversation = {"recipient":this.toString(), "mesages":Thread.messages(this.toString()) };
+                conversations.push(conversation);
+            });
+            return conversations;
+        },
+        messages: function(recipient){
+            var all_messages = $.parseJSON(localStorage.messages),
             messages = [];
             
             $.each(all_messages,function(i){
